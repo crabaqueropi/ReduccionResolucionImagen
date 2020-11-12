46d9efe975680987255eceacf22a0b5621ddfd1c
crabaqueropi <crabaqueropi@unal.edu.co>
2020-11-11 19:28:04 -0500

    q

--- ReducirImagen_CUDA.cu
+++ ReducirImagen_CUDA.cu
@@ -378,17 +378,9 @@ void reducirMatriz9x9a2x2(int imgR[9][9], int imgG[9][9], int imgB[9][9], int ou
     algoritmo2Para4K(R8x8, G8x8, B8x8, outR, outG, outB);
 }
 
-__global__ void reduccion720(int *imgRAux, int *imgGAux, int *imgBAux, int *outRAux, int *outGAux, int *outBAux, int *numeroColumnasImg, int *NUMTHREADS)
+__global__ void reduccion720(int *imgR, int *imgG, int *imgB, int *outR, int *outG, int *outB, int *numeroColumnasImg, int *NUMTHREADS)
 {
     int threadId = threadIdx.x + blockIdx.x * blockDim.x;
-
-    int **imgR = &imgRAux;
-    int **imgG = &imgGAux;
-    int **imgB = &imgBAux;
-
-    int **outR = &outRAux;
-    int **outG = &outGAux;
-    int **outB = &outBAux;
 
     if (*NUMTHREADS<=240){
         int filaInicial, filaFinal; //, threadId = *(int *)args;

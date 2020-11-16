61c25666718960d9b2e0a2573ffe3f6cb76acd33
crabaqueropi <crabaqueropi@unal.edu.co>
2020-11-16 16:52:11 -0500

    2 bloque 4 hilos por bloqu

--- ReducirImagen_CUDA.cu
+++ ReducirImagen_CUDA.cu
@@ -859,7 +859,7 @@ int main(int argc, char **argv)
     // Launch add() kernel on GPU with N blocks
 
 
-    BLOCKSPERGRID=1; //quitar
+    BLOCKSPERGRID=2; //quitar
     NUMTHREADSPerBlock=4; //quitar
     reduccion720<<<BLOCKSPERGRID, NUMTHREADSPerBlock>>>(d_imgR, d_imgG, d_imgB, d_outR, d_outG, d_outB, numeroColumnasImg, NUMTHREADS, rows, cols, outRows, outCols);
 
